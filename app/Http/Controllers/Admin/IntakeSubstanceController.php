ece6df1df871ec1e89ae27d744757a1dfaa28d4f
krishna mishra <er.krishna.mishra@gmail.com>
2020-11-18 17:32:33 +0530

    sms checking

--- app/Http/Controllers/Admin/IntakeSubstanceController.php
+++ app/Http/Controllers/Admin/IntakeSubstanceController.php
@@ -17,8 +17,63 @@ class IntakeSubstanceController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function send_twilio_text_sms($id, $token, $from, $to, $body)
+    {
+        $url = "https://api.twilio.com/2010-04-01/Accounts/".$id."/SMS/Messages";
+        $data = array (
+            'From' => $from,
+            'To' => $to,
+            'Body' => $body,
+        );
+        $post = http_build_query($data);
+        $x = curl_init($url );
+        curl_setopt($x, CURLOPT_POST, true);
+        curl_setopt($x, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($x, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($x, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($x, CURLOPT_USERPWD, "$id:$token");
+        curl_setopt($x, CURLOPT_POSTFIELDS, $post);
+        $y = curl_exec($x);
+        // echo "<pre>";print_r(curl_getinfo($x));"</pre>";exit;
+        curl_close($x);
+echo "sss";
+        echo "<pre>";print_r($y);"</pre>";exit;
+        return $y;
+    }
+
     public function index()
     {
+
+$id = "ACbcfbb403ad0a2267c6e559cf9c7c8119";
+$token = "c25fd668e797404fc58c5d1b197c9506";
+$url = "https://api.twilio.com/2010-04-01/Accounts/$id/SMS/Messages";
+$from = "36599909";
+$to = "+919026574061"; // twilio trial verified number
+$body = "using twilio rest api from Fedrick";
+$data = array (
+    'From' => $from,
+    'To' => $to,
+    'Body' => $body,
+);
+$post = http_build_query($data);
+$x = curl_init($url );
+curl_setopt($x, CURLOPT_POST, true);
+curl_setopt($x, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($x, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($x, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+curl_setopt($x, CURLOPT_USERPWD, "$id:$token");
+curl_setopt($x, CURLOPT_POSTFIELDS, $post);
+$y = curl_exec($x);
+// echo "<pre>";print_r($y);"</pre>";exit;
+curl_close($x);
+var_dump($post);
+var_dump($y);
+
+
+        $this->send_twilio_text_sms("ACbcfbb403ad0a2267c6e559cf9c7c8119", "c25fd668e797404fc58c5d1b197c9506", "+97336599909", "+919026574061", "Testing api please ignore");
+die;
+
 		$intake_subs = Diet::exists();
 
 		if ($intake_subs) {
