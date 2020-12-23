82d2c00564c9ba23f072a894d3d7900a6888ee2e
krishna mishra <er.krishna.mishra@gmail.com>
2020-12-23 19:11:33 +0530

    requestion client view chat

---
+++ resources/views/backend/admin/complaints/nutritionist/index.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+@section('head')
+<link href="{{asset('backend/assets/css/datatables.min.css')}}" rel="stylesheet">
+@endsection
+@section('content')
+<div class="row">
+	<div class="col-md-12">
+		<nav aria-label="breadcrumb " class="ms-panel-custom">
+			<ol class="breadcrumb pl-0">
+				<li class="breadcrumb-item"><a href="/"><i class="material-icons">home</i> Home</a></li>
+				<li class="breadcrumb-item active" aria-current="page">Complaints List</li>
+			</ol>
+		</nav>
+		@include('backend.admin.includes.flashmessage')
+	</div>
+	<div class="col-md-12">
+		<div class="ms-panel">
+			<div class="ms-panel-header">
+				<h6>Complaints List</h6>
+			</div>
+			<div class="ms-panel-body">
+				<div class="table-responsive">
+					<table id="data-table-18" class="table table-striped thead-primary w-100"></table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
+@push('scripts')
+<script>
+	var dataSet18 = [
+	@foreach($complaints as $request)
+	[ "{{ $no++ }}" ,"{{ $request->nutritionist_name }}"," {{ $request->client_name}}<br>{{ $request->client_id}}","{{ $request->reason }}",  @if(!$request->status)"Pending" @else "Approved" @endif],
+	@endforeach
+	];
+	var tablepackage = $('#data-table-18').DataTable( {
+		data: dataSet18,
+		columns: [
+		{ title: "Id" },
+		{ title: "Nutritionist Name" },
+		{ title: "Client" },
+		{ title: "Description" },
+		{ title: "Complaint Status" },
+		],
+
+	});
+</script>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script type="text/javascript">
+	$( document ).ready(function() {
+		setTimeout(function() {
+			$('.alert-success').fadeOut('fast');
+		}, 2200);
+	});
+</script>
+<script type="text/javascript">
+	function submitform(no){
+		swal({
+			title: "Are you sure?",
+			text: "Once deleted, you will not be able to recover this Package!",
+			icon: "warning",
+			buttons: true,
+			dangerMode: true,
+		})
+		.then((willDelete) => {
+			if (willDelete) {
+				document.getElementById('delete-form'+no).submit();
+			}
+		});
+	}
+</script>
+@endpush
