3767b81d11a70068786840087118851ee4fb14ee
krishna mishra <er.krishna.mishra@gmail.com>
2021-01-15 17:05:00 +0530

    notification panel

--- resources/views/backend/admin/controls/notifications/index.blade.php
+++ resources/views/backend/admin/controls/notifications/index.blade.php
@@ -25,9 +25,24 @@
 					<div class="col-sm-2">
 					</div>
 					<div class="col-sm-9">
-						<a href="{{route('send-push-notification')}}" class='btn btn-primary btnpro'>Send push notification</a>
-						<a href="{{route('send-in-chat-broadcast')}}" class='btn btn-success btnpro'>Send in-chat broadcast</a>
+						<a href="{{route('send-push-notification')}}" class='btn btn-primary btnpro'>Send general push notification</a>
+						<a href="{{route('send-personalized-push-notification')}}" class='btn btn-primary btnpro'>Send personalized push notification</a>
 						<a href="{{route('notifications-history')}}" class='btn btn-danger btnpro'>Notifications History</a>
+					</div>
+				</div>
+			</div>
+			<hr>
+			<div class="ms-panel-header">
+				<h6>Broadcasts:</h6>
+			</div>
+			<div class="ms-panel-body">
+				<div class="row">
+					<div class="col-sm-2">
+					</div>
+					<div class="col-sm-9">
+						<a href="{{route('send-in-chat-broadcast')}}" class='btn btn-success btnpro'>Send general in-chat broadcast</a>
+						<a href="{{route('send-personalized-in-chat-broadcast')}}" class='btn btn-success btnpro'>Send personalized in-chat broadcast</a>
+
 						<a href="{{route('broadcasts-history')}}" class='btn btn-info btnpro'>Broadcasts History</a> 
 					</div>
 				</div>
