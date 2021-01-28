b1b8eda61b6c12256a1fb1f5b61782f2e841b786
krishna <er.krishna.mishra@gmail.com>
2021-01-28 12:40:15 +0530

    permission chnaged

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -202,7 +202,7 @@
           <div class="grid">
             @foreach($packages as $package)
             <figure class="effect-ming">
-              <img src="{{asset('uploads/packages/'.$package->image)}}" alt="img01" class="img-fluid">
+              <img src="{{asset('uploads/packages/dashboard/'.$package->image_full)}}" alt="img01" class="img-fluid">
               <div class="gridtitle">{{ $package->name_arabic }}</div>
               <figcaption>
                 <h2> {{ $package->name_arabic }} </h2>
@@ -216,7 +216,7 @@
                     <div class="row">
                       <div class="col-sm-4">
                         <div class="popupimg">
-                          <img src="{{asset('uploads/packages/'.$package->image)}}" alt="image" class="img-fluid">
+                          <img src="{{asset('uploads/packages/dashboard/'.$package->image_full)}}" alt="image" class="img-fluid">
                         </div>
                       </div>
                       <div class="col-sm-8">
