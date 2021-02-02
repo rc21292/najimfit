7a4f9805c2d29a983e3ccf9c1dddee49ecfd667f
krishna <er.krishna.mishra@gmail.com>
2021-02-02 16:34:14 +0530

    Assign permissions to admin and staff

--- app/Http/Controllers/Admin/TableController.php
+++ app/Http/Controllers/Admin/TableController.php
@@ -24,10 +24,10 @@ class TableController extends Controller
 
   function __construct()
   {
-    $this->middleware('permission:admin-table-list|admin-table-edit', ['only' => ['index','show']]);
+    /*$this->middleware('permission:admin-table-list|admin-table-edit', ['only' => ['index','show']]);
     $this->middleware('permission:admin-table-store', ['only' => ['store']]);
     $this->middleware('permission:admin-table-edit', ['only' => ['edit']]);
-    $this->middleware('permission:admin-table-clients', ['only' => ['allclients']]);
+    $this->middleware('permission:admin-table-clients', ['only' => ['allclients']]);*/
     $this->middleware(['permission:tables']);
     $this->middleware(['permission:assign-tables']);
     
