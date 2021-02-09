843a9c06500a10b65cb8c0e368541dfc4b6c9364
krishna <er.krishna.mishra@gmail.com>
2021-02-09 16:33:35 +0530

    nutritionist chat page chnaged

--- resources/views/backend/admin/chat/nut_chat.blade.php
+++ resources/views/backend/admin/chat/nut_chat.blade.php
@@ -139,7 +139,9 @@
                                         <form id="chat-form">
                                             <ul class="ms-list-flex mb-0">
                                                 <li class="ms-chat-input">
-                                                    <input type="text" id="content" name="content" placeholder="Enter Message" value="" >
+
+                                                    <textarea rows="7" id="content" name="content" placeholder="Enter Message" ></textarea>
+
                                                 </li>
                                                 <li style="margin-top: -16px; margin-left: 3px;">
                                                     <button type="submit" class="btn btn-primary">Send</button>
