0f3108725c6a0b1081568b05a434c2381a9c51e4
Prashant <prashant@n2rtech>
2021-02-15 17:15:59 +0530

    uploading packages file

--- resources/views/backend/admin/teams/employees.blade.php
+++ resources/views/backend/admin/teams/employees.blade.php
@@ -35,7 +35,7 @@
             $renewal_workout = DB::table('nutritionist_clients')->where('nutritionist_id',$user->id)->where('workout_status','posted')->count();
             @endphp
             <tr>
-              <th scope="row">1</th>
+              <th scope="row">{{$loop->iteration}}</th>
               <td>{{$user->name}}</td>
               <td>{{$active_clients}}</td>
               <td>{{ $posted_tables }}</td>
@@ -82,7 +82,7 @@
             $renewal_workout = DB::table('nutritionist_clients')->where('nutritionist_id',$user->id)->where('workout_status','posted')->count();
             @endphp
             <tr>
-              <th scope="row">1</th>
+              <th scope="row">{{$loop->iteration}}</th>
               <td>{{$user->name}}</td>
               <td>{{$active_clients}}</td>
               <td>{{ $posted_tables }}</td>
@@ -97,24 +97,48 @@
     </div>
   </div>
   @endif
-  @if($role_name == 'Customer Service')
+ @if($role_name == 'Staff')
   <div class="ms-panel">
     <div class="ms-panel-header">
-      <h6>No User assigned to this team</h6>
+      <h6>All Staff</h6>
     </div>
-  </div>
-  @endif
-  @if($role_name == 'Subscription Coordinators')
-  <div class="ms-panel">
-    <div class="ms-panel-header">
-      <h6>No User assigned to this team</h6>
-    </div>
-  </div>
-  @endif
-  @if($role_name == 'Supervisors')
-  <div class="ms-panel">
-    <div class="ms-panel-header">
-      <h6>No User assigned to this team</h6>
+    <div class="ms-panel-body">
+      <p class="ms-directions"></p>
+      <div class="table-responsive">
+        <table class="table table-hover thead-primary">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Staff Name</th>
+              <th scope="col">Active Clients</th>
+              <th scope="col">Tables</th>
+              <th scope="col">Workouts</th>
+              <th scope="col">Renewals</th>
+              <th scope="col">Renewal Tables</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($users as $user)
+            @php
+            $active_clients = DB::table('nutritionist_clients')->where('nutritionist_id',$user->id)->count();
+            $posted_tables = DB::table('nutritionist_clients')->where('nutritionist_id',$user->id)->where('table_status','due')->count();
+            $posted_workouts = DB::table('nutritionist_clients')->where('nutritionist_id',$user->id)->where('workout_status','due')->count();
+            $renewal_table = DB::table('nutritionist_clients')->where('nutritionist_id',$user->id)->where('table_status','posted')->count();
+            $renewal_workout = DB::table('nutritionist_clients')->where('nutritionist_id',$user->id)->where('workout_status','posted')->count();
+            @endphp
+            <tr>
+              <th scope="row">{{$loop->iteration}}</th>
+              <td>{{$user->name}}</td>
+              <td>{{$active_clients}}</td>
+              <td>{{ $posted_tables }}</td>
+              <td>{{ $posted_workouts }}</td>
+              <td>{{ $renewal_table }}</td>
+              <td>{{ $renewal_workout }}</td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
   @endif
