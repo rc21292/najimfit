8aa5d7934e36b949e204a9685a0f18b06d4c184b
fatehgotra <gfateh@n2rtechnologies.in>
2021-02-23 12:05:01 +0530

    Update nut_chat.blade.php

--- resources/views/backend/admin/chat/nut_chat.blade.php
+++ resources/views/backend/admin/chat/nut_chat.blade.php
@@ -419,11 +419,11 @@
         });
     </script>
 
-     <script type="text/javascript" src="https://unpkg.com/mic-recorder-to-mp3@2.2.2/dist/index.js"></script>
+    <script type="text/javascript" src="https://unpkg.com/mic-recorder-to-mp3@2.2.2/dist/index.js"></script>
     <script type="text/javascript">
 
         var timer = 0,
-        timeTotal = 62400,
+        timeTotal = 61800,
         timeCount = 20,
         timeStart = 0,
         cFlag;
@@ -444,8 +444,8 @@
             timeStart = new Date().getTime();
 
             recorder.start().then(() => {
-                animateUpdate();
                 $('#error-message').html('');
+                animateUpdate();
                 stopbutton.textContent = 'Send Recording';
                 $(".cancelRecordingButton").show();
                 $(".emojionearea").hide();
@@ -470,10 +470,13 @@
                     lastModified: Date.now()
                 });
 
+                let duration = buffer.duration;
+            console.log('dura',duration);
+            document.write('size',duration);
+
                 var fd=new FormData();
-                receiver_id: '{{$receptorUser->id}}'
-                fd.append("receiver_id", "{{@$receptorUser->id}}");
-                fd.append("sender_id", "{{@$senderUser->id}}");
+                fd.append("receiver_id", "{{$receptorUser->id}}");
+                fd.append("sender_id", "{{@Auth::user()->id}}");
                 fd.append("audio_data",blob, "voice-recording");
                 $.ajax({
                     headers: {
@@ -526,7 +529,7 @@
             console.log('min',min);
             var  cancelRecording = $("#cancelRecording").val();
             console.log('cancel',cancelRecording);
-            if ((percentage == 62400) && (cancelRecording != '1')) {
+            if ((percentage == 61800) && (cancelRecording != '1')) {
                 stopRecording();
             }
             $('#pbar_innerdiv').css("width", x + "%");
