3629650b1de8ca9f405a5e5f9d1d3f8c4bec2736
fatehgotra <gfateh@n2rtechnologies.in>
2021-02-23 12:09:10 +0530

    Update view_chat.blade.php

--- resources/views/backend/admin/client_chats/view_chat.blade.php
+++ resources/views/backend/admin/client_chats/view_chat.blade.php
@@ -5,6 +5,13 @@
 @role('Admin')
 @section('content')
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/emojionearea/3.4.2/emojionearea.min.css" integrity="sha512-vEia6TQGr3FqC6h55/NdU3QSM5XR6HSl5fW71QTKrgeER98LIMGwymBVM867C1XHIkYD9nMTfWK2A0xcodKHNA==" crossorigin="anonymous" />
+<style type="text/css">
+    .circle-icon {
+    background: #ffc0c0;
+    padding:10px;
+    border-radius: 50%;
+}
+</style>
 <div id="app" class="ui main container" >
     <div class="ui grid">
         <div class="row">           
@@ -60,10 +67,19 @@
                                             <ul class="ms-list-flex mb-0">
                                                 <li class="ms-chat-input">
                                                     <textarea rows="-3" id="content" name="content" placeholder="Enter Message" ></textarea>
+                                                    <div id="pbar_outerdiv" style=" display: none;  width: 300px; height: 15px; border: 1px solid grey; z-index: 1; position: relative; border-radius: 5px; -moz-border-radius: 5px;">
+                                                        <div id="pbar_innerdiv" style="background-color: lightblue; z-index: 2; height: 100%; width: 0%;"></div>
+                                                        <div id="pbar_innertext" style="z-index: 3; position: absolute; top: -4px; left: 0; height: 100%; color: black; font-weight: bold; text-align: center;">0&nbsp;s</div>
+                                                        <p>Recording in progress....</p>
+                                                    </div>
                                                 </li>
                                                 <li style="margin-top: -16px; margin-left: 3px;">
-                                                    <button type="submit" class="btn btn-primary">Send</button>
+                                                    <a class="RecordButton" style="font-size: 20px;" href="javascript:void(0);"><i class="fa fa-microphone circle-icon" aria-hidden="true"></i></a>
+                                                <button style="display: none;" title="Record a Message" class=" sendRecordingButton btn btn-primary"><i class="fa fa-microphone" aria-hidden="true"></i> </button>
+                                                 <button style="display: none;" class=" cancelRecordingButton btn btn-primary">Cancel Recording</button>
+                                                    <button type="submit" class="sendMessage btn btn-primary">Send</button>
                                                 </li>
+                                                <input type="hidden" value="0" id="cancelRecording" name="cancelRecording">
                                             </ul>
                                         </form>
                                     </div>
@@ -202,8 +218,13 @@
                         }
                         chat_element += '<div class="media-body">';
 
-                        chat_element += '<div class="ms-chat-text"><p>';                
+                        chat_element += '<div class="ms-chat-text"><p>';
+
+                        if (chat_content != ' ') {                
                         chat_element += chat_content;
+                    }else{
+                         chat_element += '<audio id="audio" controls><source src="'+ childData.recording +'" type="audio/mp3"></audio>';
+                    }
 
                         chat_element += '</p></div>';
 
@@ -358,7 +379,6 @@
                 };
 
                 storageRef.child('images/' + file.name).put(file, metadata).then(function (snapshot) {
-                    console.log(snapshot);
                     snapshot.ref.getDownloadURL().then(function (url) {
                         document.getElementById('file_path').value = url;
                         document.getElementById('file_name').value = file.name;
@@ -386,4 +406,159 @@
                 });
             });
         </script>
-        @endpush
+
+ <script type="text/javascript" src="https://unpkg.com/mic-recorder-to-mp3@2.2.2/dist/index.js"></script>
+    <script type="text/javascript">
+
+        var timer = 0,
+        timeTotal = 61800,
+        timeCount = 20,
+        timeStart = 0,
+        cFlag;
+
+        const button = document.querySelector('.RecordButton');
+        const stopbutton = document.querySelector('.sendRecordingButton');
+        const cancelbutton = document.querySelector('.cancelRecordingButton');
+        const recorder = new MicRecorder({
+            bitRate: 128
+        });
+
+        button.addEventListener('click', startRecording);
+
+        function startRecording() {
+
+            clearTimeout(timer);
+            cFlag = true;
+            timeStart = new Date().getTime();
+
+            recorder.start().then(() => {
+                $('#error-message').html('');
+                animateUpdate();
+                stopbutton.textContent = 'Send Recording';
+                $(".cancelRecordingButton").show();
+                $(".emojionearea").hide();
+                $(".sendMessage").hide();
+                $("#pbar_outerdiv").show();
+                $("#pbar_outerdiv").trigger('click');
+                $('.sendRecordingButton').show();
+                $('.RecordButton').hide();
+                stopbutton.classList.toggle('btn-danger');
+                stopbutton.removeEventListener('click', startRecording);
+                stopbutton.addEventListener('click', stopRecording);
+            }).catch((e) => {
+                $('#error-message').html('Microphone access denied...');
+                console.error(e);
+            });
+        }
+
+        function stopRecording() {
+            recorder.stop().getMp3().then(([buffer, blob]) => {
+                const file = new File(buffer, 'music.mp3', {
+                    type: blob.type,
+                    lastModified: Date.now()
+                });
+
+                let duration = buffer.duration;
+
+                var fd=new FormData();
+                 fd.append("receiver_id", "{{@$receptorUser->id}}");
+                fd.append("sender_id", "{{@$senderUser->id}}");
+                fd.append("audio_data",blob, "voice-recording");
+                $.ajax({
+                    headers: {
+                        'X-CSRF-TOKEN': $("meta[name=csrf-token]").attr('content')
+                    },
+                    url: "{{ route('save-recording') }}",
+                    data: fd,
+                    method: 'POST',
+                    contentType: false,
+                    processData: false,
+                    success: function(data) {
+                    }
+                });
+
+                $(".sendRecordingButton").hide();
+                $(".cancelRecordingButton").hide();
+                $(".RecordButton").show();
+                $(".emojionearea").show();
+                $(".sendMessage").show();
+                $("#pbar_outerdiv").hide();
+                $(".cancelRecordingButton ").hide();
+                button.removeEventListener('click', stopRecording);
+                button.addEventListener('click', startRecording);    
+            }).catch((e) => {
+                console.error(e);
+            });
+        }
+
+        function sendRecording() {
+            recorder.stop().getMp3().then(([buffer, blob]) => {
+                const file = new File(buffer, 'music.mp3', {
+                    type: blob.type,
+                    lastModified: Date.now()
+                });
+            }).catch((e) => {
+                console.error(e);
+            });
+        }
+
+
+        function updateProgress(percentage) {
+            var x = (percentage/timeTotal)*100,
+            y = x.toFixed(3);
+            var totalSec= (percentage / 1000);
+            var min = parseInt(totalSec/60);
+            var sec = parseInt(totalSec%60);
+            var hr= parseInt(min/60);
+            min = parseInt(min % 60);
+            var  cancelRecording = $("#cancelRecording").val();
+            if ((percentage == 61800) && (cancelRecording != '1')) {
+                stopRecording();
+            }
+            $('#pbar_innerdiv').css("width", x + "%");
+            $('#pbar_innertext').css("left", x + "%").text(min+":"+sec + "");
+        }
+
+        function animateUpdate() {
+            var perc = new Date().getTime() - timeStart;
+            if(perc < timeTotal) {
+                updateProgress(perc);
+                timer = setTimeout(animateUpdate, timeCount);
+            } else {
+                updateProgress(timeTotal);
+            }
+        }
+
+        $(document).ready(function() {
+            $('#pbar_outerdiv').click(function() {
+                var  cancelRecording = $("#cancelRecording").val();
+                if (cancelRecording) {
+                }else{
+                    if (cFlag == undefined) {
+                        clearTimeout(timer);
+                        cFlag = true;
+                        timeStart = new Date().getTime();
+                        animateUpdate();
+                    }
+                }
+            });
+        }); 
+
+        $(".cancelRecordingButton").click(function () 
+        {
+            $("#cancelRecording").val(1);
+            stopbutton.textContent = 'Start recording';
+            stopbutton.classList.remove('btn-danger');
+            stopbutton.classList.add('btn-primary');
+            $(".emojionearea").show();
+            $(".sendMessage").show();
+            $(".sendRecordingButton").hide();
+            $(".RecordButton").show();
+            $("#pbar_outerdiv").hide();
+            $(".cancelRecordingButton ").hide();
+            stopbutton.removeEventListener('click', stopRecording);
+            stopbutton.addEventListener('click', startRecording);    
+        });
+    </script>
+
+    @endpush
